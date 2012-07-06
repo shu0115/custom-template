4af417d5a80ef087fbbd7325d5d34d1afdddaa67
shu0115 <raisondetre0115@gmail.com>
2012-07-06 16:51:25 +0900

    update

--- lib/custom-template.rb
+++ lib/custom-template.rb
@@ -1,7 +1,22 @@
+# coding: utf-8
 require "custom-template/version"
+require 'rails/generators'
 
 module Custom
-  module Template
-    # Your code goes here...
+  module Generators
+    class TemplateGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../custom-template", __FILE__)
+      desc "This generator custom template for scaffold"
+
+      #-------------------#
+      # generate_template #
+      #-------------------#
+      def generate_template
+        directory "templates/erb", "lib/templates/erb", recursive: true
+        directory "templates/haml", "lib/templates/haml", recursive: true
+        directory "templates/rails", "lib/templates/rails", recursive: true
+      end
+
+    end
   end
 end
